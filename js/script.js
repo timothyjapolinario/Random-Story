42eedcfb5d348ac6ce5ef2cef9596a012395c062
JayJay <timothyjapolinario@gmail.com>
2022-04-21 23:50:19 +0800

    update story

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ characters = ["Mark", "Jasper", "Josh", "Angeliree", "Gilbert", "Dayang"];
 actions = ["kumain", "tumae", "takbo", "umupo", "suminghot", "sumapi"];
 objects = ["tae", "tinapay", "burat", "kamote", "kambing", "kalabaw", "fita", "martilyo"];
 adjectives = ["manyakis", "malibog", "duwag", "bobo", "supot", "patay gutom", "sabog", "angry birds"];
-
-let story = `Merong dalawang magkaaway, ang pangalan nila'y ${getRandomElement(characters)} at ${getRandomElement(characters)}`;
+character1 = getRandomElement(characters)
+character2 = getRandomElement(characters)
+let story = `
+Merong dalawang magkaaway, ang pangalan nila'y ${character1} na ${getRandomElement(adjectives)} at ${character2} na ${getRandomElement(adjectives)}.\n
+Mahilig si ${character1} sa ${getRandomElement(objects)}.\n
+Si ${character2} naman ay mahilig ${getRandomElement(actions)} ng ${getRandomElement(objects)} `;
 body.innerText = story;
